fd0ac27997cb17fd10f78306995a6faf117aaf91
Brian Hildebrand <bhildebrand@zendesk.com>
2017-12-15 14:46:20 -0800

    Fixed padding at top of page templates.

--- js/brian.js
+++ js/brian.js
@@ -21,8 +21,8 @@ $( document ).ready(function() {
 	  setTimeout(function(){
 	    $('#instafeed').slick({
 	      infinite: true,
-	      slidesToShow: 2,
-	      slidesToScroll: 3,
+	      slidesToShow: 4,
+	      slidesToScroll: 4,
 	      prevArrow: '<div class="circle previous-instagram"><div class="arrow-previous"></div></div>',
 	      nextArrow: '<div class="circle next-instagram"><div class="arrow-next"></div></div>',
 	      infinite: false,
